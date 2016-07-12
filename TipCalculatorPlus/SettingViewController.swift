15eb0e1ac8d0d8c77e4017890c18bd0bd1f5794b
akishinagawa <akishinagawa@hotmail.com>
2016-07-11 21:36:40 -0700

    Initial files upload
    Initial Commit

---
+++ TipCalculatorPlus/SettingViewController.swift
@@ -0,0 +1,101 @@
+//
+//  SettingViewController.swift
+//  TipCalculatorPlus
+//
+//  Created by Akifumi Shinagawa on 7/5/16.
+//  Copyright © 2016 BlurryBlue. All rights reserved.
+//
+
+
+import UIKit
+
+import Foundation
+
+
+
+
+
+class SettingViewController: UIViewController {
+
+    let defaultminTipPercentageMax = 10.0
+    let defaultMaxTipPercentageMax = 50.0
+    
+    
+    var minTipPercentage = 0.0
+    var maxTipPercentage = 0.0
+    
+    
+    
+    @IBOutlet weak var minPercentgeLabel: UILabel!
+    @IBOutlet weak var maxPercentageLabel: UILabel!
+    
+ 
+    @IBOutlet weak var minPercentageSlider: UISlider!
+    @IBAction func minPercentageSliderChanged(sender: AnyObject) {
+        
+        minTipPercentage = Double(defaultminTipPercentageMax) * Double(minPercentageSlider.value)
+        
+        let percentFrmatter = NSNumberFormatter()
+        percentFrmatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        percentFrmatter.maximumFractionDigits = 1
+        percentFrmatter.positiveFormat = "0.0"
+        percentFrmatter.roundingMode = NSNumberFormatterRoundingMode.RoundHalfUp
+        
+        minPercentgeLabel.text = percentFrmatter.stringFromNumber(minTipPercentage)! + " %"
+        
+        NSUserDefaults.standardUserDefaults().setDouble(minTipPercentage, forKey: "minPercentage")
+    }
+    
+    @IBOutlet weak var maxPercentageSlider: UISlider!
+    @IBAction func maxPercentageSliderChanged(sender: AnyObject) {
+       maxTipPercentage = Double(defaultMaxTipPercentageMax - defaultminTipPercentageMax) * Double(maxPercentageSlider.value)
+        
+        let percentFrmatter = NSNumberFormatter()
+        percentFrmatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        percentFrmatter.maximumFractionDigits = 1
+        percentFrmatter.positiveFormat = "0.0"
+        percentFrmatter.roundingMode = NSNumberFormatterRoundingMode.RoundHalfUp
+        
+        maxPercentageLabel.text = percentFrmatter.stringFromNumber(maxTipPercentage)! + " %"
+        
+        NSUserDefaults.standardUserDefaults().setDouble(maxTipPercentage, forKey: "maxPercentage")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NSLog("---->>　SettingViewController: viewDidLoad")
+        
+        self.setTipPercentageValues()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    func setTipPercentageValues() {
+        
+        
+        let percentFrmatter = NSNumberFormatter()
+        percentFrmatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        percentFrmatter.maximumFractionDigits = 1
+        percentFrmatter.positiveFormat = "0.0"
+        percentFrmatter.roundingMode = NSNumberFormatterRoundingMode.RoundHalfUp
+        
+        minPercentgeLabel.text = percentFrmatter.stringFromNumber(minTipPercentage)! + " %"
+        maxPercentageLabel.text = percentFrmatter.stringFromNumber(maxTipPercentage)! + " %"
+    }
+
+
+
+
+
+
+
+}
---
+++ TipCalculatorPlus/ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  TipCalculatorPlus
+//
+//  Created by Akifumi Shinagawa on 6/12/16.
+//  Copyright © 2016 BlurryBlue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var originalPriceText: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipPriceLabel: UILabel!
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    
+    @IBOutlet weak var percentageSlider: UISlider!
+    @IBOutlet weak var splitNumberText: UITextField!
+    @IBOutlet weak var priceSplitByPeopleLabel: UILabel!
+    
+    let defaultMaxTipPercentage = 20.0
+    let defaultminTipPercentage = 5.0
+    
+    var maxTipPercentage = 0.0
+    var minTipPercentage = 0.0
+    
+    var splitNUmber = 1
+    
+    
+    @IBAction func onTipPercentageChanged(sender: AnyObject) {
+        //NSLog("---->>onTipPercentageChanged")
+        
+        self.updateValues()
+    }
+    
+    @IBAction func returnToMainView(segue: UIStoryboardSegue) {
+        NSLog("---->>　UIViewController: returnToMainView")
+        
+        self.checkPreviousData()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "goSetting" {
+
+            let settingViewController = segue.destinationViewController as! SettingViewController
+
+            settingViewController.maxTipPercentage = self.maxTipPercentage
+            settingViewController.minTipPercentage = self.minTipPercentage
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.checkPreviousData()
+        
+        NSLog("---->>　UIViewController: viewDidLoad")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        NSLog("---->>touchesBegan")
+        
+        self.view.endEditing(true)
+        self.updateValues()
+    }
+    
+    
+    
+    func checkPreviousData() {
+        
+        if (NSUserDefaults.standardUserDefaults().objectForKey("savedBefore") != nil) {
+            NSLog("---->>checkPreviousData : Yes. There is data.")
+            
+            maxTipPercentage = NSUserDefaults.standardUserDefaults().doubleForKey("maxPercentage")
+            minTipPercentage = NSUserDefaults.standardUserDefaults().doubleForKey("minPercentage")
+        }
+        else {
+            NSLog("---->>checkPreviousData : No. There is no data.")
+            
+            maxTipPercentage = defaultMaxTipPercentage
+            minTipPercentage = defaultminTipPercentage
+            
+            NSUserDefaults.standardUserDefaults().setObject("yes", forKey: "savedBefore")
+            NSUserDefaults.standardUserDefaults().setDouble(maxTipPercentage, forKey: "maxPercentage")
+            NSUserDefaults.standardUserDefaults().setDouble(minTipPercentage, forKey: "minPercentage")
+        }
+        
+        self.updateValues()
+    }
+    
+    func saveCurrentPercentage(){
+        NSUserDefaults.standardUserDefaults().setDouble(maxTipPercentage, forKey: "maxPercentage")
+        NSUserDefaults.standardUserDefaults().setDouble(minTipPercentage, forKey: "minPercentage")
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func updateValues() {
+        //NSLog("---->>calculateTip")
+
+        
+        var originalPrice = Float(originalPriceText.text!)
+        if originalPrice == nil {
+            originalPrice = 0.0
+        }
+        
+        let tipPercentage  = Float(minTipPercentage) + (Float(maxTipPercentage - minTipPercentage) * Float(percentageSlider.value))
+        
+        let tipPrice = originalPrice! * tipPercentage / 100
+        
+        let totalPrice = originalPrice! + tipPrice
+        
+        
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.locale = NSLocale.currentLocale()
+        
+        tipPriceLabel.text = currencyFormatter.stringFromNumber(tipPrice)
+        totalPriceLabel.text = currencyFormatter.stringFromNumber(totalPrice)
+        
+        let percentFrmatter = NSNumberFormatter()
+        percentFrmatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        percentFrmatter.maximumFractionDigits = 1
+        percentFrmatter.positiveFormat = "0.0"
+        percentFrmatter.roundingMode = NSNumberFormatterRoundingMode.RoundHalfUp
+
+        tipPercentageLabel.text = percentFrmatter.stringFromNumber(tipPercentage)! + " %"
+        
+        splitNUmber = Int(splitNumberText.text!)!
+        
+        let splittedPrice = totalPrice / Float(splitNUmber)
+        
+        priceSplitByPeopleLabel.text = currencyFormatter.stringFromNumber(splittedPrice)
+        
+    }
+    
+
+
+}
+
